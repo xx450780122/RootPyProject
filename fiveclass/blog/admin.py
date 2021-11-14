041310358159075c6e4fe1d9e4513560257ad31d
H <H@H.com>
2021-11-14 15:48:04 +0800

    Admin bind.

--- fiveclass/blog/admin.py
+++ fiveclass/blog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Post,Category, Tag
+
+class PostAdmin(admin.ModelAdmin):
+    pass
+
+class CategoroyAdmin(admin.ModelAdmin):
+    pass
+
+class TagAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Post)
+admin.site.register(Tag)
+admin.site.register(Category)
--- fiveclass/config/admin.py
+++ fiveclass/config/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.admin.sites import site
 
 # Register your models here.
+from .models import Link, SideBar
+
+@admin.register(Link)
+class LinkAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(SideBar)
+class SideBarAdmin(admin.ModelAdmin):
+    pass
